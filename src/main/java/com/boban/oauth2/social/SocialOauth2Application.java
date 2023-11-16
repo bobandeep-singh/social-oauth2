d1b69c4ac5f49bf6230a87a0c404d3588902a60e
Bobandeep Singh <bobandeep_singh@apple.com>
2023-11-17 01:35:31 +0530

    OAuth2.0 implementation using Github

---
+++ src/main/java/com/boban/oauth2/social/SocialOauth2Application.java
@@ -0,0 +1,13 @@
+package com.boban.oauth2.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialOauth2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/boban/oauth2/social/controllers/DemoController.java
@@ -0,0 +1,16 @@
+package com.boban.oauth2.social.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class DemoController {
+
+    @GetMapping("/demo")
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok("Hello Boban");
+    }
+}
